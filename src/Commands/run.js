c60df0e19e9475d84fc20729d1c32c928354dc04
Freddie John Millman <freddie.millman@icloud.com>
2018-07-24 10:41:03 +0100

    Change sendMessage function to reply function

--- src/Commands/run.js
+++ src/Commands/run.js
@@ -7,36 +7,36 @@ const run = () => (ctx) => {
     ctx.session.participants = [];
     ctx.session.operation = 'ready';
 
-    ctx.sendMessage(ctx.from_chat_id, 'You have 5 minutes to drop your instagram username.');
-    sleep(240).then(ctx.sendMessage(ctx.from_chat_id, 'You have 1 minute to drop your instagram username.'));
+    ctx.reply('You have 5 minutes to drop your instagram username.');
+    sleep(240).then(ctx.reply('You have 1 minute to drop your instagram username.'));
     sleep(60).then((ctx) => {
-        ctx.sendMessage(ctx.from_chat_id, 'Let us run!');
+        ctx.reply('Let us begin!');
         ctx.session.operation = 'in progress';
         ctx.session.startTime = Date.now();
-        ctx.sendMessage(ctx.from_chat_id, 'You have 30 minutes to work your way through the following list:');
+        ctx.reply('You have 30 minutes to work your way through the following list:');
     });
 
     for (let i = 0; i < ctx.session.participants.length; i++) {
-        ctx.sendMessage(ctx.from_chat_id, `${ctx.session.participants[i].instagram}`);
+        ctx.reply(`${ctx.session.participants[i].instagram}`);
     }
 
     let timeLeft = 30;
 
     while (ctx.session.participants.length = 0 || timeLeft > 0) {
         sleep(300).then((ctx) => {
-            ctx.sendMessage(ctx.from_chat_id, 'The following have yet to complete the list:');
+            ctx.reply('The following have yet to complete the list:');
             for (let i = 0; i < ctx.session.participants.length; i++) {
-                ctx.sendMessage(ctx.from_chat_id, `@${ctx.session.participants[i].username}`);
+                ctx.reply(`@${ctx.session.participants[i].username}`);
             }
             if (timeLeft =- 5 > 0)
-                ctx.sendMessage(ctx.from_chat_id, `You have ${timeLeft} minutes left.`);
+                ctx.reply(`You have ${timeLeft} minutes left.`);
         });
     }
 
     if (ctx.session.participants.length > 0) {
-        ctx.sendMessage(ctx.from_chat_id, 'This is a warning for the following who did not complete the list:');
+        ctx.reply('This is a warning for the following who did not complete the list:');
         for (let i = 0; i < ctx.session.participants.length; i++) {
-            ctx.sendMessage(ctx.from_chat_id, `@${ctx.session.participants[i].username}`);
+            ctx.reply(`@${ctx.session.participants[i].username}`);
         }
     }
 
